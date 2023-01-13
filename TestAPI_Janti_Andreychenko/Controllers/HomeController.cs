45a28bb02ca28afb11921109f56529799280289f
Михаил Андрейченко <andreychenkome@gmail.com>
2023-01-13 09:41:59 +0500

    Добавьте файлы проекта.

---
+++ TestAPI_Janti_Andreychenko/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System.Data;
+using System.Globalization;
+
+namespace TestAPI_Janti_Andreychenko.Controllers
+{
+    [Route("/TimeManager/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        [Route("/GetTime")]
+        [HttpGet]
+        public async Task<ActionResult<string>> GetTime()
+        {
+            var timeManager = new TimeManager();
+
+            return await timeManager.GetTime();
+        }
+
+        [Route("/ConvertDate")]
+        [HttpGet]
+        public async Task<ActionResult<string>> ConvertDate(string date)
+        {
+            var str = ;
+            return str;
+        }
+
+
+        [HttpPost("/SetTimeZone")]
+        public async Task<ActionResult<string>> SetTimeZone(string date)
+        {
+
+            return string.Empty;
+        }
+
+
+    }
+}
---
+++ TestAPI_Janti_Andreychenko/TimeManager.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TestAPI_Janti_Andreychenko
+{
+    public class TimeManager
+    {
+        private static TimeZoneInfo currentTimeZone;
+
+        public TimeManager()
+        {
+            currentTimeZone = TimeZoneInfo.Utc;
+        }
+
+        public async Task<string> GetTime()
+        {
+            string result = "";
+
+
+
+            return result;
+        }
+
+        public async Task<bool> SetTimeZone(string time)
+        {
+            var result = true;
+
+            return result;
+        }
+
+        public async Task<string> ConvertDate(string time)
+        {
+            string result = "";
+
+            return result;
+        }
+    }
+}
